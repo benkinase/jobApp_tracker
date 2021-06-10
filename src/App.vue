68996c47402635f7d5c7248b5e5eb998b130584f
gbenaimako <gbenimako@gmail.com>
2021-06-10 15:48:58 +0200

    App.vue and main.ts updated

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Header />
+
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header/index.vue";
+
+export default defineComponent({
+  components: { Header },
+});
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  margin: 5rem auto;
+  width: 95%;
+}
+.error__alert {
+  color: #f52369;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s linear;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import { devtools } from "@/utils";
+import { store } from "./store/modules/Jobs";
+import "./assets/sass/global.scss";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(devtools);
+app.use(router);
+app.mount("#app");
